69b03f0dbe92924677ebb3ded094e9ca8124cbe8
tlei <terry.haotian.lei@gmail.com>
2018-08-19 18:02:04 -0700

    Add stub for graph renderer

---
+++ server/client/js/graph_renderer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class GraphRenderer {
+  constructor(canvas) {
+    this._canvas = _canvas;
+  }
+
+  redraw(people) {
+    if (people.length == 0) return;
+
+    let ctx = this._canvas.getContext('2d');
+
+    for (let person of people) {
+
+    }
+  }
+}
