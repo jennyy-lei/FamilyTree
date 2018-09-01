b6f55d3f5d9c3d5a04c6a1f9d54bb6502e2b02be
tlei <terry.haotian.lei@gmail.com>
2018-09-01 14:46:52 -0700

    Add UIDs to models.

--- server/client/js/models.js
+++ server/client/js/models.js
@@ -2,19 +2,38 @@
 
 // Person object
 export class Person {
-  constructor(fname, lname, dateOfBirth, relationships = []) {
+  constructor(fname, lname, dateOfBirth, {relationships = [], id}) {
     this.firstName = fname;
     this.lastName = lname;
     this.dateOfBirth = dateOfBirth;
     this.relationships = relationships;
+    // If no ID is supplied, assign a unique ID to the person, so it
+    // can be easily referenced.
+    this.id = id || personIdGenerator.next().value;
+  }
+
+  addRelationship(relationship) {
+    this.relationships.push(relationship);
   }
 }
 
 // Stores relationships + pairs name
 export class Relationship {
-  constructor(kind, children, metadata = {}) {
+  constructor(kind, leftId, rightId, {metadata = {}, id}) {
     this.kind = kind;
     this.children = children;
     this.metadata = metadata;
+    // If no ID is supplied, assign a unique ID to the relationship,
+    // so it can be easily referenced.
+    this.id = id || relationshipIdGenerator.next().value;
   }
 }
+
+// Creates an incrementally decreasing ID starting at startIndex for
+// each successive call of the generator.
+function * idGenerator(startIndex) {
+  while (true) yield startIndex--;
+}
+
+let relationshipIdGenerator = idGenerator(0);
+let personIdGenerator = idGenerator(0);
