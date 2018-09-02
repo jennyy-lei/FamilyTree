4141a299e5bb71dcdf09a69c2ae8c1cc65350623
tlei <terry.haotian.lei@gmail.com>
2018-09-02 11:04:36 -0700

    add person manager class

---
+++ server/client/js/person_repository.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Stores all people and relationships between those people within
+// in the application. Provides convenience methods for managing
+// relationships and doing lookups.
+export class PersonRepository {
+  constructor() {
+    this._people = {};
+    this.relationships = [];
+  }
+
+  addPerson(person) {
+    if (this._people.has(person.id)) {
+      console.log(
+          `Could not add person, ID ${person.id} already exists`);
+      return false;
+    }
+
+    this._people[person.id] = person;
+    return true;
+  }
+
+  addRelationship(relationship) {
+    if (!this._people.has(relationship.leftId)
+        || !this._people.has(relationship.rightId)) {
+      console.log(
+          'Could not add relationship, left or right person ' +
+              'ID does not exist');
+      return false;
+    }
+
+    this.relationships.push(relationship);
+    return true;
+  }
+
+  get people() { return this._people.values(); }
+}
